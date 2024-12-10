97b197fed05febf57a75e8f8d2693b99c657933b
JiRath <garethaguilar@gmail.com>
2024-12-09 19:22:16 -0500

    Implement BP render target to texture function

---
+++ Source/SSJ2024/Rendering/SSJRenderingLibrary.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SSJRenderingLibrary.h"
+
+#include "Engine/TextureRenderTarget2D.h"
+
+UTexture2D* USSJRenderingLibrary::CreateTextureFromRenderTarget(UObject* WorldContextObject, UTextureRenderTarget2D* RT)
+{
+	return RT->ConstructTexture2D(WorldContextObject, "BlockTexture", RF_Transient);
+}
---
+++ Source/SSJ2024/Rendering/SSJRenderingLibrary.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SSJRenderingLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SSJ2024_API USSJRenderingLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable, meta=(WorldContext="WorldContextObject"))
+	static UTexture2D* CreateTextureFromRenderTarget(UObject* WorldContextObject, UTextureRenderTarget2D* RT);
+};
